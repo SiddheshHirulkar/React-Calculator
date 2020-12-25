2480d793d6fe99be3bf478fb1a956d6c10a83df6
SiddheshHirulkar <siddhu3096@gmail.com>
2020-12-25 21:00:06 +0530

    Updates

---
+++ precache-manifest.23de31e5980162b005e89cac7afe2a05.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e98664b3294053c10d0397db89c022a",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "7338ab9aad791ba43d34",
+    "url": "/React-Calculator/static/css/2.2dcf3477.chunk.css"
+  },
+  {
+    "revision": "135519865cf60480a65e",
+    "url": "/React-Calculator/static/css/main.6807cf4c.chunk.css"
+  },
+  {
+    "revision": "7338ab9aad791ba43d34",
+    "url": "/React-Calculator/static/js/2.517324a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.517324a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135519865cf60480a65e",
+    "url": "/React-Calculator/static/js/main.ebd8ba44.chunk.js"
+  },
+  {
+    "revision": "095e8fa0c910cb73c76b",
+    "url": "/React-Calculator/static/js/runtime-main.f95e9a27.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/React-Calculator/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/React-Calculator/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/React-Calculator/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/React-Calculator/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/React-Calculator/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
